91ce04cc5d5f709b41de1e82c1917f6ce0b68c46
gurrl9823 <gurrl9823@gmail.com>
2025-08-22 21:26:10 +0900

    init project

---
+++ src/test/java/com/example/test_automation/TestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
