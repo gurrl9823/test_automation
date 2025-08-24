fe49ffb0ccdcba9481f889517f6c7f3ff2c0bffc
gurrl9823 <gurrl9823@gmail.com>
2025-08-24 22:09:08 +0900

    테스트 성공, 커버리지 미충족 테스트

--- src/main/java/com/example/test_automation/service/UserService.java
+++ src/main/java/com/example/test_automation/service/UserService.java
@@ -6,6 +6,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public String getUserInfo(Long userId) {
+
+        String a = "A";
+        String b = "B";
+        String c = "C";
+        if("aaa".equals(a) && "bbb".equals(b)) {
+            c = "ccc";
+        }
+
         String userKey = "123";
         return "홍길동 25세";
     }
